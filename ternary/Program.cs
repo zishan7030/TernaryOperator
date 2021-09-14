04007261d366efa9856eed5db27b1802b8db2840
Dell <Dell@ZISHAN>
2021-09-14 13:53:38 +0530

    Add project files.

---
+++ ternary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            int withdrawl;
+            int balance = 10000;
+            Console.WriteLine("Enter Your Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Your Account has {0} Amount", balance);
+            Console.WriteLine("Enter Your Amount");
+            withdrawl = Convert.ToInt32(Console.ReadLine());
+            
+
+            balance = (withdrawl > balance )? (balance) :( balance -withdrawl);
+
+            Console.WriteLine("Your Balance is " + balance);
+
+        }
+    }
+}
